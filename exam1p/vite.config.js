64b811d4f690b1c002c594418cb04ba9a89fd955
Andres0319 <121038655@upq.edu.mx>
2023-10-04 16:43:20 -0600

    examp 1p

--- exam1p/vite.config.js
+++ exam1p/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,14 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            // Importamos Bootstrap 5
+            resolve:{
+                alias:{
+                    '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+                }
+            },
+            // Fin Importamos Bootstrap 5
+
         }),
     ],
 });
